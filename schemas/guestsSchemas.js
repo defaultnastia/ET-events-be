c12dc1332ac24afe5a1f1616558e50f57d3d8c99
defaultnastia <anastayaaa@gmail.com>
2024-09-22 00:56:00 +0200

    added source to the validation schema

--- schemas/guestsSchemas.js
+++ schemas/guestsSchemas.js
@@ -1,8 +1,9 @@
 import Joi from "joi";
-import { emailRegExp } from "../constants/guestConstants.js";
+import { emailRegExp, sourceTypes } from "../constants/guestConstants.js";
 
 export const addGuestSchema = Joi.object({
   name: Joi.string().min(3).max(40).required(),
   email: Joi.string().pattern(emailRegExp).required(),
   birthDate: Joi.date().less(Date.now()).required(),
+  source: Joi.string().pattern(sourceTypes).required(),
 });
